c3d1954c6eb9c00368136817f0b3db4917265002
Abdulkhaaliq <55828788+Abdulkhaaliq@users.noreply.github.com>
2019-10-02 15:18:56 +0200

    add

---
+++ ChuckNorrisJokesSln/ChuckNorrisJokes/Program.cs
@@ -0,0 +1,25 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+
+            string randomJokeString = await jokeGenerator.GetRandomJoke();
+
+            Console.WriteLine(randomJokeString);
+            string[] catergories = await jokeGenerator.GetCategories();
+
+            //  Console.WriteLine("Hello World!");
+            for (int index = catergories.Length-1; index >=0; index--)
+            {
+                Console.WriteLine(catergories[index]);
+            }
+        }
+    }
+}
